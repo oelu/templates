1743904f8215091f00470d400c4dacd3df4cec57
oelu <olivierschenk@gmail.com>
2014-07-12 12:43:11 +0200

    added logging and exeption check for import statements

--- template.py
+++ template.py
@@ -4,12 +4,21 @@ template description
 Usage:
       template.py (-f <file> | --file <file>)
 
+Options:
+    -h, --help      print out help message
+    -v, --verbose   activate verbose output
 """
 __author__ = 'olivier'
+__title__ = 'template.py'
+__version__ = '0.1'
 
-# import statements
-from docopt import docopt
-
+try:
+    from docopt import docopt
+    import logging as log
+except ImportError as ioex:
+    log.error("Could not import a required module!")
+    log.error(ioex)
+    sys.exit(1)
 
 def main():
     """
@@ -17,8 +26,17 @@ def main():
     """
     # gets arguments from docopt
     arguments = docopt(__doc__)
-    # assigns docopt argumen
-    print arguments
+    # assigns docopt arguments
+    verbose = docopt['--verbose']
+
+    # set loglevel
+    if verbose:
+        log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        log.info("Verbose output activated.")
+    else:
+        log.basicConfig(format="%(levelname)s: %(message)s")
+    log.info("Script was started with arguments: ")
+    log.info(arguments)
 
 
 if __name__ == "__main__":
