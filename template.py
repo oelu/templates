73b30042451b8a3d56995ad2040bbadbd6117785
oelu <olivierschenk@gmail.com>
2015-01-07 20:50:28 +0100

    import sys, fixed doctest layout

--- template.py
+++ template.py
@@ -1,13 +1,14 @@
-#!/usr/local/bin/python2.7
 """ template.py
-template description
+Template description.
+
 Usage:
-      template.py (-f <file> | --file <file>)
+    template.py [options]
 
 Options:
     -h, --help      print out help message
     -v, --verbose   activate verbose output
 """
+
 __author__ = 'olivier'
 __title__ = 'template.py'
 __version__ = '0.1'
@@ -15,10 +16,12 @@ __version__ = '0.1'
 try:
     from docopt import docopt
     import logging as log
+    import sys
 except ImportError as ioex:
     log.error("Could not import a required module!")
     log.error(ioex)
     sys.exit(1)
+
 
 def main():
     """
@@ -27,7 +30,7 @@ def main():
     # gets arguments from docopt
     arguments = docopt(__doc__)
     # assigns docopt arguments
-    verbose = docopt['--verbose']
+    verbose = arguments['--verbose']
 
     # set loglevel
     if verbose:
@@ -35,8 +38,11 @@ def main():
         log.info("Verbose output activated.")
     else:
         log.basicConfig(format="%(levelname)s: %(message)s")
+
     log.info("Script was started with arguments: ")
     log.info(arguments)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
